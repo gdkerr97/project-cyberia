37b671c7a4005439e32500d50ba97577b38b05b2
Gianfrancesco <gianfrancesco.devicienti@gmail.com>
2024-01-20 21:14:21 +0100

    aggiornati i dto

--- cyberia-app-fe/src/dto/commentdto.ts
+++ cyberia-app-fe/src/dto/commentdto.ts
@@ -2,17 +2,7 @@
 
 export class CommentDTO{
 
-    id: number;
-    content: string;
-    created_at: string;
-    post_id: number;
-    ip_address: number;
 
-    constructor(id: number, content: string, created_at: string, post_id: number, ip_address: number){
-        this.id = id;
-        this.content = content;
-        this.created_at = created_at;
-        this.post_id = post_id;
-        this.ip_address = ip_address;
+    constructor(id?: number, content?: string, created_at?: string, post_id?: number, ip_address?: number){
     }
 }
--- cyberia-app-fe/src/dto/postdto.ts
+++ cyberia-app-fe/src/dto/postdto.ts
@@ -1,16 +1,7 @@
 /* PostDTO stesso del backend */
 export class PostDTO{
-    id: number;
-    subject: string;
-    description: string;
-    created_at: string;
-    ip_address: number;
 
-    constructor(id: number, subject: string, description: string, created_at: string, ip_address: number){
-        this.id = id;
-        this.subject = subject;
-        this.description = description;
-        this.created_at = created_at;
-        this.ip_address = ip_address;
+
+    constructor(id?: number, subject?: string, description?: string, created_at?: string, ip_address?: number){
     }
 }
