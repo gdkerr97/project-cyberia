68436eb164598a2a59f3d52db085e9f83602d555
Gianfrancesco <gianfrancesco.devicienti@gmail.com>
2024-02-06 18:36:45 +0100

    some fix and updates

--- cyberia-app-fe/src/app/threads/header/header.component.ts
+++ cyberia-app-fe/src/app/threads/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit{
 
+  constructor(private activatedRoute: ActivatedRoute){}
+
+  id: number = this.activatedRoute.snapshot.params['id'];
   gifs: string[] = ['lain', 'lain_computer', 'lain_computer2', 'pc', 'wired'];
   file: string = '../../../assets/gif/';
 
---
+++ cyberia-app-fe/src/app/threads/services/threads.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThreadsService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllPosts(): Observable<any>{
+    return this.http.get<any>(environment.APIEndpoint + '')
+  }
+
+
+}
